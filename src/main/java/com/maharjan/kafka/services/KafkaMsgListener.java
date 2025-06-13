a9a3689f69061a173fda397161ba39d2c35167a0
Amit Maharjan <maharjanamit8@gmail.com>
2025-06-13 16:38:05 -0400

    Added 6 consumer instances to listen the message from 5 partitions.

--- src/main/java/com/maharjan/kafka/services/KafkaMsgListener.java
+++ src/main/java/com/maharjan/kafka/services/KafkaMsgListener.java
@@ -10,8 +10,33 @@ import org.springframework.stereotype.Service;
 public class KafkaMsgListener {
     Logger logger = LoggerFactory.getLogger(KafkaMsgListener.class);
 
-    @KafkaListener(topics = Constants.KAFKA_TOPIC_PARTITION_5, groupId = Constants.KAFKA_GROUP_ID_1)
-    public void listen(String msg) {
-        logger.info("Consumer consumed the msg = {}", msg);
+    @KafkaListener(topics = Constants.KAFKA_TOPIC_PARTITION_5, groupId = Constants.KAFKA_GROUP_ID)
+    public void listen1(String msg) {
+        logger.info("Consumer1 consumed the msg = {}", msg);
+    }
+
+    @KafkaListener(topics = Constants.KAFKA_TOPIC_PARTITION_5, groupId = Constants.KAFKA_GROUP_ID)
+    public void listen2(String msg) {
+        logger.info("Consumer2 consumed the msg = {}", msg);
+    }
+
+    @KafkaListener(topics = Constants.KAFKA_TOPIC_PARTITION_5, groupId = Constants.KAFKA_GROUP_ID)
+    public void listen3(String msg) {
+        logger.info("Consumer3 consumed the msg = {}", msg);
+    }
+
+    @KafkaListener(topics = Constants.KAFKA_TOPIC_PARTITION_5, groupId = Constants.KAFKA_GROUP_ID)
+    public void listen4(String msg) {
+        logger.info("Consumer4 consumed the msg = {}", msg);
+    }
+
+    @KafkaListener(topics = Constants.KAFKA_TOPIC_PARTITION_5, groupId = Constants.KAFKA_GROUP_ID)
+    public void listen5(String msg) {
+        logger.info("Consumer5 consumed the msg = {}", msg);
+    }
+
+    @KafkaListener(topics = Constants.KAFKA_TOPIC_PARTITION_5, groupId = Constants.KAFKA_GROUP_ID)
+    public void listen6(String msg) {
+        logger.info("Consumer6 consumed the msg = {}", msg);
     }
 }
--- src/main/java/com/maharjan/kafka/utils/Constants.java
+++ src/main/java/com/maharjan/kafka/utils/Constants.java
@@ -2,5 +2,5 @@ package com.maharjan.kafka.utils;
 
 public class Constants {
     public static final String KAFKA_TOPIC_PARTITION_5 = "kafka-topic-partition-5";
-    public static final String KAFKA_GROUP_ID_1 = "kafka-group-id-1";
+    public static final String KAFKA_GROUP_ID = "kafka-group-id";
 }
